093aa66927bb985a234f2797358d633a2ee342c1
Ryan Rowe <ryanrrowe@gmail.com>
2020-05-11 19:08:40 -0700

    loading spinner while waiting for stripe

--- src/components/StripeElementsWrapper.tsx
+++ src/components/StripeElementsWrapper.tsx
@@ -3,6 +3,8 @@ import { IAppState } from '../store/app';
 import { Stripe } from '@stripe/stripe-js';
 import { stripePromiseSelector } from '../store/cms';
 import { useSelector } from 'react-redux';
+import BaseLayout from '../layouts/BaseLayout';
+import Loading from './Loading';
 import React from 'react';
 
 interface Props {
@@ -14,7 +16,11 @@ const StripeElementsWrapper: React.FC<Props> = ({ children, type }) => {
 
   // do not render if stripe object is not a promise yet.
   if (Promise.resolve(stripePromise) !== stripePromise) {
-    return null;
+    return (
+      <BaseLayout>
+        <Loading />
+      </BaseLayout>
+    );
   }
 
   return <Elements stripe={stripePromise}>{children}</Elements>;
