065bbbbe3a8093b08df5458983b7e0ecdeef0ae6
Ryan Rowe <ryanrrowe@gmail.com>
2020-06-20 16:13:28 -0700

    add support for footer logos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import { IAppState } from '../store/app';
 import { IConfig, INavItem } from '../common/types';
 import { reverse } from '../common/router';
-import { useContent } from '../store/cms';
+import { useContent, useContentImage } from '../store/cms';
 import { useIsSmall } from '../common/hooks';
 import { useSelector } from 'react-redux';
 import { useTheme } from '@material-ui/core';
@@ -20,6 +20,7 @@ const Footer: React.FC = () => {
   const navDonate = useContent('nav_donate');
   const navDrive = useContent('nav_drive');
   const navLink = useContent('nav_link');
+  const footerLogo = useContentImage('footer_logo');
   const config = useSelector<IAppState, IConfig>((state) => state.cms.config);
   const { driverForm, driverFormName, donationEnabled } = config;
   const footerNavItems: INavItem[] = [{ name: navPurchase, url: reverse('products') }];
@@ -32,11 +33,12 @@ const Footer: React.FC = () => {
     footerNavItems.push({ name: navDrive, url: driverFormName ? `/${driverFormName}` : reverse('drivers') });
   }
 
-  footerNavItems.push({ name: 'Partner Login', url: 'https://airtable.com' });
-
   return (
-    <div className={styles.footer} style={{ backgroundColor: `${primaryColor[50]}80` }}>
-      <div className={classNames(styles.links, { [styles.small]: isSmall })}>
+    <div
+      className={classNames(styles.container, { [styles.small]: isSmall })}
+      style={{ backgroundColor: `${primaryColor[50]}80` }}
+    >
+      <div className={styles.links}>
         {footerNavItems.map((item, index) => (
           <Link key={`${item.name}${index}`} href={item.url} className={styles.link}>
             {item.name}
@@ -49,6 +51,9 @@ const Footer: React.FC = () => {
         )}
       </div>
       <ContactUs />
+      <div className={styles.logo}>
+        {footerLogo && <img src={footerLogo.url} alt={footerLogo.alt} className={styles.logoImg} />}
+      </div>
     </div>
   );
 };
