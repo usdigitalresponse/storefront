a1581b2988a4334827080939d7219f11cbd32b52
Ryan Rowe <ryanrrowe@gmail.com>
2020-06-23 22:37:44 -0700

    handle waitlist more elegantly and prevent duplicate orders

--- src/api/create-order.js
+++ src/api/create-order.js
@@ -101,16 +101,15 @@ exports.handler = async (event, context) => {
       { typecast: true },
     );
 
-    // Note: temporaily disable this logic until further notice
     // prevent duplicate orders
-    // let existingOrders = await fetchTable('Orders', {
-    //   view: DEFAULT_VIEW,
-    //   filterByFormula: `AND({Name} = "${orderIntent.fullName}", {Email} = "${orderIntent.email}")`,
-    // });
+    let existingOrders = await fetchTable('Orders', {
+      view: DEFAULT_VIEW,
+      filterByFormula: `AND({Name} = "${orderIntent.fullName}", {Email} = "${orderIntent.email}")`,
+    });
 
-    // if (existingOrders && existingOrders.length > 0) {
-    //   throw new Error('Only one order per person!');
-    // }
+    if (existingOrders && existingOrders.length > 0) {
+      throw new Error('Only one order per person!');
+    }
 
     // process order items
     const items = await base('Order Items').create(
@@ -164,7 +163,7 @@ exports.handler = async (event, context) => {
     }
 
     try {
-      const formattedOrder = await getFormattedOrder(order.fields['Order ID'], 'All Orders');
+      const formattedOrder = await getFormattedOrder(order.fields['Order ID'], DEFAULT_VIEW);
       await Promise.all([
         sendOrderConfirmationEmailUser(formattedOrder),
         sendOrderConfirmationEmailPartner(formattedOrder),
