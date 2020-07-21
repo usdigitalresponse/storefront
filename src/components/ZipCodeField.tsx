cf9abffa9950e4a9743766d87d54dfdc7d0eabc2
sabrichu <sabrichu@gmail.com>
2020-07-21 11:06:31 -0700

    Add zipcode schedule to confirmation page/email and update README with email instructions

--- src/components/ZipCodeField.tsx
+++ src/components/ZipCodeField.tsx
@@ -1,11 +1,13 @@
 import { Autocomplete } from '@material-ui/lab';
 import { IAppState } from '../store/app';
-import { TextField, TextFieldProps } from '@material-ui/core';
+import { TextField, TextFieldProps, Typography } from '@material-ui/core';
 import { ZipcodeScheduleMap } from '../common/types';
 import { useSelector } from 'react-redux';
 import { zipcodeListSelector, zipcodeSchedulesSelector } from '../store/cms';
+import Content from './Content';
 import React, { ChangeEvent, useState } from 'react';
 import ScheduleView from './ScheduleView';
+import styles from './ZipCodeField.module.scss';
 
 const ZipCodeField: React.FC<TextFieldProps> = (props) => {
   const zipcodeList = useSelector<IAppState, string[]>(zipcodeListSelector);
@@ -26,8 +28,14 @@ const ZipCodeField: React.FC<TextFieldProps> = (props) => {
         className={props.className}
         renderInput={(params) => <TextField {...props} {...params} />}
       />
-
-      <ScheduleView variant="body2" schedules={zipcodeSchedules[selectedZipcode]} />
+      {zipcodeSchedules[selectedZipcode] && zipcodeSchedules[selectedZipcode].length && (
+        <>
+          <Typography variant="body2" className={styles.note}>
+            <Content id="zipcode_delivery_schedule_label" defaultText="Delivery is available for this zipcode" />
+          </Typography>
+          <ScheduleView variant="body2" schedules={zipcodeSchedules[selectedZipcode]} className={styles.note} />
+        </>
+      )}
     </>
   );
 };
