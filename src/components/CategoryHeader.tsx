b48cc32149cec67cf48cac4f7f60eda7d5747440
sabrichu <sabrichu@gmail.com>
2020-11-05 09:08:06 -0800

    Add anchor links for category headers

--- src/components/CategoryHeader.tsx
+++ src/components/CategoryHeader.tsx
@@ -2,6 +2,7 @@ import { Typography } from '@material-ui/core';
 import { useIsSmall } from '../common/hooks';
 import React from 'react';
 import classNames from 'classnames';
+import slug from 'slug';
 import styles from './CategoryHeader.module.scss';
 
 interface Props {
@@ -15,6 +16,9 @@ const CategoryHeader: React.FC<Props> = ({ categoryName, description, centered }
 
   return (
     <div className={classNames(styles.container, centered && styles.centered, isSmall && styles.small)}>
+      {/* Using a hidden div positioned higher up on the page as the navigation location for URL fragments,
+      as navigating directly to the header causes it to be hidden by the sticky navigation bar */}
+      {categoryName && <div className={styles.anchor} id={slug(categoryName)}></div>}
       <Typography variant="h4" className={styles.header}>
         {categoryName}
       </Typography>
